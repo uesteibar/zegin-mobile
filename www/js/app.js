ee3ee583ac3d9a5c0a9fe32efac8ba891d6c1d02
uesteibar@gmail.com <uesteibar@gmail.com>
2014-12-29 21:05:07 +0100

    add geolocation plugin

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-var zegin = angular.module('zegin', ['ionic']);
+var zegin = angular.module('zegin', ['ionic', 'ngCordova']);
 
 var options = {};
 options.api = {};
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,40 +1,65 @@
-zegin.controller('TimelineCtrl', function ($scope, EventsService, $window) {
+zegin.controller('TimelineCtrl', function ($scope, EventsService, $window, $ionicPlatform, $cordovaGeolocation) {
     $scope.events = [];
-    $scope.refreshEvents = function(){
-        EventsService.getAllEvents().then(function (res) {
-            $scope.events = res.data;
-            console.log($scope.events);
-        }, function (err) {
-            $window.alert(err);
+
+    $scope.refreshEvents = function () {
+        $ionicPlatform.ready(function () {
+
+            // TODO - query round km
+            //                $cordovaPlugin.someFunction().then(success, error);
+            console.log("device is ready!");
+
+            var posOptions = {
+                timeout: 10000,
+                enableHighAccuracy: false
+            };
+            console.log('Getting current position');
+            $cordovaGeolocation
+                .getCurrentPosition(posOptions)
+                .then(function (position) {
+                    var lat = position.coords.latitude
+                    var long = position.coords.longitude
+
+                    alert(lat + ", " + long);
+
+
+                    EventsService.getAllEvents().then(function (res) {
+
+                        $scope.events = res.data;
+                        console.log($scope.events);
+                    }, function (err) {
+                        $window.alert(err);
+                    })
+                        .finally(function () {
+                            $scope.$broadcast('scroll.refreshComplete');
+                        });
+
+                }, function (err) {
+                    // error
+                });
+
+
+
+
+
         });
     };
-    
+
+    $scope.GotoEventDetails = function (id) {
+        $window.location = "#/timeline/" + id;
+    };
+
+
+
     $scope.refreshEvents();
-    
-    $scope.refreshEventsPull = function () {
-        EventsService.getAllEvents().then(function (res) {
-            $scope.events = res.data;
-            console.log($scope.events);
-        }, function (err) {
-            $window.alert(err);
-        })
-        .finally(function(){
-            $scope.$broadcast('scroll.refreshComplete');
-        });
-    };
-    
-    $scope.GotoEventDetails= function(id){
-        $window.location="#/timeline/"+id;
-    };
-    
-    
+
+
 });
 zegin.controller('EventDetailsCtrl', function ($scope, EventsService, $stateParams, $ionicHistory) {
     $scope.event = {
-        name:"",
-        date:0
+        name: "",
+        date: 0
     };
-    $scope.getEvent = function(id){
+    $scope.getEvent = function (id) {
         EventsService.getEvent(id).then(function (res) {
             $scope.event = res.data;
             console.log($scope.event);
@@ -42,12 +67,12 @@ zegin.controller('EventDetailsCtrl', function ($scope, EventsService, $statePara
             $window.alert(err);
         });
     };
-    
+
     $scope.getEvent($stateParams.id);
-    
-    $scope.goBack = function(){
+
+    $scope.goBack = function () {
         $ionicHistory.goBack();
     };
-    
-    
+
+
 });
